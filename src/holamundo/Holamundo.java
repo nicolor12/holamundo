c20e9870ecc585e7338877773c21e86ce0dc6760
Estudiante <Estudiante@10.203.2.96>
2018-08-13 08:53:43 -0500

    Cambie el mensaje

--- src/holamundo/Holamundo.java
+++ src/holamundo/Holamundo.java
@@ -15,7 +15,7 @@ public class Holamundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("hola mundo");
+       System.out.println("Eyyyyy");
        int wjndfkjewhjhbkjwkjewhfkjhfkw=87687;
        System.out.println(wjndfkjewhjhbkjwkjewhfkjhfkw);     
        
